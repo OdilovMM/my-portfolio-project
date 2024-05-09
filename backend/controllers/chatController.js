ac7941baafc5640606a9326c108cab8275081629
OdilovMM <dssrinman@gmail.com>
2024-05-09 12:28:14 +0500

    PART5: seller to customer, customer to seller connection

--- backend/controllers/chatController.js
+++ backend/controllers/chatController.js
@@ -5,14 +5,93 @@ const formidable = require("formidable");
 const sellerCustomerModel = require("../models/chat/sellerCustomerModel");
 
 class chatController {
+  // addChatFriend = async (req, res) => {
+  //   const { sellerId, userId } = req.body;
+
+  //   try {
+  //     if (sellerId !== "") {
+  //       const seller = await Seller.findById(sellerId);
+  //       const customer = await Customer.findById(userId);
+
+  //       const checkSeller = await sellerCustomerModel.findOne({
+  //         $and: [
+  //           {
+  //             myId: {
+  //               $eq: userId,
+  //             },
+  //           },
+  //           {
+  //             myFriends: {
+  //               $elemMatch: {
+  //                 fdId: sellerId,
+  //               },
+  //             },
+  //           },
+  //         ],
+  //       });
+  //       if (!checkSeller) {
+  //         await sellerCustomerModel.updateOne(
+  //           {
+  //             myId: userId,
+  //           },
+  //           {
+  //             $push: {
+  //               myFriends: {
+  //                 fdId: sellerId,
+  //                 name: seller.shopInfo?.shopName,
+  //                 image: seller.image,
+  //               },
+  //             },
+  //           }
+  //         );
+  //       }
+  //       const checkCustomer = await sellerCustomerModel.findOne({
+  //         $and: [
+  //           {
+  //             myId: {
+  //               $eq: sellerId,
+  //             },
+  //           },
+  //           {
+  //             myFriends: {
+  //               $elemMatch: {
+  //                 fdId: userId,
+  //               },
+  //             },
+  //           },
+  //         ],
+  //       });
+  //       if (!checkSeller) {
+  //         await sellerCustomerModel.updateOne(
+  //           {
+  //             myId: sellerId,
+  //           },
+  //           {
+  //             $push: {
+  //               myFriends: {
+  //                 fdId: userId,
+  //                 name: customer.name,
+  //                 image: "",
+  //               },
+  //             },
+  //           }
+  //         );
+  //       }
+  //       console.log(checkCustomer);
+  //       console.log(checkSeller);
+  //     }
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+
   addChatFriend = async (req, res) => {
     const { sellerId, userId } = req.body;
 
     try {
       if (sellerId !== "") {
         const seller = await Seller.findById(sellerId);
-        const customer = await Customer.findById(userId);
-
+        const user = await Customer.findById(userId);
         const checkSeller = await sellerCustomerModel.findOne({
           $and: [
             {
@@ -29,9 +108,59 @@ class chatController {
             },
           ],
         });
+        if (!checkSeller) {
+          await sellerCustomerModel.updateOne(
+            {
+              myId: userId,
+            },
+            {
+              $push: {
+                myFriends: {
+                  fdId: sellerId,
+                  name: seller.shopInfo?.shopName,
+                  image: seller.image,
+                },
+              },
+            }
+          );
+        }
 
-        console.log(checkSeller);
+        const checkCustomer = await sellerCustomerModel.findOne({
+          $and: [
+            {
+              myId: {
+                $eq: sellerId,
+              },
+            },
+            {
+              myFriends: {
+                $elemMatch: {
+                  fdId: userId,
+                },
+              },
+            },
+          ],
+        });
+        if (!checkCustomer) {
+          await sellerCustomerModel.updateOne(
+            {
+              myId: sellerId,
+            },
+            {
+              $push: {
+                myFriends: {
+                  fdId: userId,
+                  name: user.name,
+                  image: "",
+                },
+              },
+            }
+          );
+        }
       }
+
+      console.log(checkCustomer);
+      console.log(checkSeller);
     } catch (error) {
       console.log(error);
     }
