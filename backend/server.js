264069e3f502304e887e4a67e5ef9a629c5ad660
OdilovMM <dssrinman@gmail.com>
2024-05-31 00:47:56 +0500

    Add: frontend url

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ const dashboardRouter = require("./routes/dashboard/dashboardRoutes");
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",
+      "https://eshop-frontend-fawn.vercel.app",
       "https://admin-dashboard-eshop.vercel.app",
     ],
     credentials: true,
