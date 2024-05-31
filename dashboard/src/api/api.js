84d5c8da1e9e0353c48aabb97e248dd441a1c42d
OdilovMM <dssrinman@gmail.com>
2024-05-31 10:10:39 +0500

    Add: update

--- dashboard/src/api/api.js
+++ dashboard/src/api/api.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
-const production = "https://multi-vendor-shop.onrender.com";
 
 const api = axios.create({
-  baseURL: `${production}/api/v1`,
+  baseURL: `https://multi-vendor-shop.onrender.com/api/v1`,
 });
 
 export default api;
